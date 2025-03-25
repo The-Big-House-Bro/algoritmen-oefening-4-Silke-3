ff8d8da25525eaf28589b74cf8e17c213b7958e4
Silke Vandenbroucke <silke.vandenbroucke@leerling.athenea.be>
2025-03-25 09:19:20 +0100

    huiswerk

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,14 @@
-def countTargetPairs():
+
+def countTargetPairs(nums, target):
+    pairs = 0
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] < target:
+                pairs += 1
+    return pairs
+
+nums = [6, -6, -7, 2, 3]
+target = 2
+result = countTargetPairs(nums, target)
+print(result)
